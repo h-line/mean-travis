7e03258f8c4a443d1a5d807af4d47b50eaed2573
Lauri Hälinen <lauri.halinen@gmail.com>
2015-02-02 12:42:24 +0200

    test

--- tests/first.spec.js
+++ tests/first.spec.js
@@ -1,8 +1,12 @@
 var request = require('request');
 
-it("should respond with test", function(done) {
-  request("http://localhost:3000/test", function(error, response, body){
-    expect(body).toEqual("test");
-    done();
-  });
+describe('test suite',function() {
+
+	it("should respond with test", function(done) {
+	  request("http://localhost:3000/", function(error, response, body){
+	    expect(body).toEqual("test");
+	    done();
+	  });
+	});
+
 });
